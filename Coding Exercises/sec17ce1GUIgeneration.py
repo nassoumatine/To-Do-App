c44bba93f58a57512872da47cde4a49f1359b9b1
nassoumatine <152454715+nakaassoumatine@users.noreply.github.com>
2024-09-10 08:06:55 -0400

    Implement the Add button

--- Coding Exercises/sec17ce1GUIgeneration.py
+++ Coding Exercises/sec17ce1GUIgeneration.py
@@ -17,7 +17,7 @@ window.close()
 
 # Another GUI
 label = sg.Text("What are dolphins?")
-option1 = sg.Radio("Amphibians", group_id="question1")
+option1 = sg.Radio("Amphibians", group_id="question1")  # .Radio helps to chose one option from many.
 option2 = sg.Radio("Fish", group_id="question1")
 option3 = sg.Radio("Mammals", group_id="question1")
 option4 = sg.Radio("Birds", group_id="question1")
@@ -25,9 +25,9 @@ option4 = sg.Radio("Birds", group_id="question1")
 window = sg.Window("File Compressor",
                    layout=[[label],
                            [option1],
-                            [option2],
-                            [option3],
-                            [option4],
+                           [option2],
+                           [option3],
+                           [option4],
                            ])
 
 window.read()
--- gui.py
+++ gui.py
@@ -1,11 +1,23 @@
 import functions
-import FreeSimpleGUI as sg # "as sg"helps avoid writing FreeSimpleGUI since its long to write.
+import FreeSimpleGUI as sg  # "as sg"helps avoid writing FreeSimpleGUI since its long to write.
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box], [add_button]])
-window.read()
-print("Hello")
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:  # to control the killing when we close the GUI
+            break
 window.close()
