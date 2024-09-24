8c612d43523a99c35841a5681bdc6469cda7feb1
Naka Assoumatine <rassoumatine@mail.wlu.edu>
2024-09-23 20:20:02 -0400

    Implement the Edit button

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,24 +1,20 @@
 # This app is a GUI that compresses files and puts them in a folder
 
-import FreeSimpleGUI as sq
+import FreeSimpleGUI as sg
 
-label1 = sq.Text("Select files to compress:")
-input1 = sq.Input()
-choose_button1 = sq.FilesBrowse("Choose")
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
 
-label2 = sq.Text("Select destination folder:")
-input2 = sq.Input()
-choose_button2 = sq.FolderBrowse("Choose")
+label2 = sg.Text("Select files to compress:")
+input2 = sg.Input()
+choose_button2 = sg.FilesBrowse("Choose")
 
-compress_button = sq.Button("Compress")
-
-window = sq.Window("File Compressor", layout=[[label1, input1, choose_button1],
+compress_button = sg.Button("Compress")
+s
+window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
                                               [label2, input2, choose_button2],
                                               [compress_button]])
-
-while True:
-    event, values = window.read()
-    pass
 
 window.read()
 window.close()
--- gui.py
+++ gui.py
@@ -1,40 +1,45 @@
 import functions
-import FreeSimpleGUI as sg  # "as sg"helps avoid writing FreeSimpleGUI since its long to write.
+import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button('Add')
-list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+input_box = sg.InputText(tooltip="Enter todo", key='todo entered')
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='existing todos',
                       enable_events=True, size=[45, 10])
+
 edit_button = sg.Button('Edit')
-
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
-
-window = sg.Window('My To-Do App', layout=layout, font=("Helvetica", 20))
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvetica', 20))
 while True:
-    event, values = window.read()
-    print(values)
+    event, values = window.read()  # This s a tupple of the event(button pressed), and a dict with jet
+# index and value the text entered
+    print(1, event)
+    print(2, values)
+    print(3, values['existing todos'])
+
     match event:
-        case 'Add':
+        case "Add":
             todos = functions.get_todos()
-            new_todo = values['todo'] + '\n'
+            new_todo = values['todo entered'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-            window['todos'].update(values=todos)   # To change the to-do in the GUI display immediately
-
-        case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            window['existing todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['existing todos'][0]
+            new_todo = values['todo entered']
 
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
-            window['todos'].update(values=todos)   # To change the to-do in the GUI display immediately
-        case 'todos':
-            window['todo'].update(value=values['todos'][0])
-        case sg.WIN_CLOSED:  # to control the killing when we close the GUI
+
+            window['existing todos'].update(values=todos)
+            # to have the edited version show in GUI automatically
+        case 'existing todos':
+            window['todo entered'].update(value=values['existing todos'][0])
+        case sg.WIN_CLOSED:
             break
 
-print('Bye')
+
 window.close()
